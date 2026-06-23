a841af80d5e7e2cc84c64df3bb57aff520bbe7dc
frypan05 <daksharma5804@gmail.com>
2026-06-24 00:07:37 +0530

    feat: Qwik language snippets+syntax highlighting.

---
+++ languages/qwik/highlights.scm
@@ -0,0 +1,24 @@
+[
+"component$"
+"useSignal"
+"useStore"
+"useTask$"
+"useVisibleTask$"
+"routeLoader$"
+"routeAction$"
+"server$"
+] @function
+
+[
+"Link"
+"Form"
+] @type
+
+[
+"useLocation"
+"useNavigate"
+] @variable
+
+[
+"$"
+] @operator
